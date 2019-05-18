535ac303ad4c8482c504e7875dd74ddf89487223
cartmandos <cartman.dos@protonmail.com>
2019-05-18 16:47:14 +0300

    originalpath prop fallback for service helper sourced tvshow widgets

--- service.py
+++ service.py
@@ -2,24 +2,18 @@
 # vim: set et sw=4 ts=4 sts=4 ff=unix fenc=utf8:
 """
 Main service module
-
 This module handles all TV Melodies services in Kodi
-
 This file is part of TV Melodies.
-
 TV Melodies is free software: you can redistribute it and/or
 modify it under the terms of the GNU General Public License as
 published by the Free Software Foundation, either version 3 of
 the License, or (at your option) any later version.
-
 TV Melodies is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with TV Melodies. If not, see <http://www.gnu.org/licenses/>.
-
 @author: smitchell6879
 @license: GPL-3.0
 """
@@ -52,6 +46,8 @@ class Monitor(xbmc.Monitor):
                     self.xml = xbmc.getInfoLabel("Window.Property(xmlfile)")
                     self.p = xbmc.getInfoLabel('Container({}).ListItem().Path'.format(xbmc.getInfoLabel('System.CurrentControlID')))
                     self.pi = xbmc.getInfoLabel('ListItem().FolderPath')
+                    if not self.p:
+                        self.p = xbmc.getInfoLabel('Container({}).ListItem.Property(originalpath)'.format(xbmc.getInfoLabel('System.CurrentControlID')))
                     if player.isPlaying():
                         if "theme" in player.getPlayingFile():
                             if self.xml != "DialogVideoInfo.xml":
